0db9601d8fa6e7b96e04457676e4059cb56959ba
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-08-05 00:34:49 +0200

    tryie13

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 const path = require('path');
 
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + './project/src/index.html'));
+  res.sendFile(path.join(__dirname + './project/dist/index.html'));
 });
 
 console.log('Console listening!');
