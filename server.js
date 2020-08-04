6a5bb0afa680e90421254f8b6f030bc7be572895
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-08-05 01:12:57 +0200

    tryie20

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://pacific-island-75677.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + './project/dist/index.html'));
