7cf53df49fbab55d05d4c9e00b9146ce6db54955
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-08-05 11:18:40 +0200

    tryie22

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://thawing-river-01959.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://pacific-island-75677.herokuapp.com/api/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
